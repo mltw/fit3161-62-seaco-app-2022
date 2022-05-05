4d4343bdb26f880c72f4b00cc81f471888c041bb
mlim0032 <mlim0032@student.monash.edu>
2022-05-05 12:27:28 +0800

    added base design for qna page

--- frontend/src/components/MainSections/QuesAns.js
+++ frontend/src/components/MainSections/QuesAns.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import { Row, Statistic, Card, Col } from 'antd';
+import { StyledCard } from '../Styled';
 
 export default function QuesAns() {
   return (
-    <div>QuesAns</div>
+    <div>
+      <h1 style={{textAlign: "center", fontSize: "25px"}}>Questions for Analysis</h1>
+      <hr></hr>
+      <br></br>
+      {/* justify="space-around"  */}
+      <Row justify="space-around" style={{padding: "0 10px"}}>
+            <StyledCard bordered={false}>
+                {/* <Card bordered={false}> */}
+                    <h3>Title</h3>
+                    <div>
+                      Tableau chart here
+                    </div>
+                {/* </Card> */}
+            </StyledCard>
+
+            <StyledCard bordered={false}>
+                   <h3>Title 2</h3>
+                    <div>
+                      Tableau chart here dlfnad;iofshdf;uioab;u
+                    </div>
+            </StyledCard>
+        </Row>
+        <Row justify='center' style={{marginTop: "20px", padding: "0 10px"}}>
+          <Col flex="auto">
+          <Card bordered={false} style={{border: "2px solid black"}}>
+                {/* <Card bordered={false}> */}
+                    <h3>Title 3</h3>
+                    <div>
+                      Tableau chart here
+                    </div>
+                {/* </Card> */}
+            </Card>
+          </Col>
+        
+        </Row>
+    </div>
   )
 }
